a60dea9f57c934ee74761fe963158531d2dc3154
Leonardo <leomanoelcoelho@gmail.com>
2024-08-05 10:09:22 -0300

    Update UserService.cs
    Implementacao padrao de seguranca

--- ScamerAPI/Scamer.Pesquisa/Autenticacao/Services/UserService.cs
+++ ScamerAPI/Scamer.Pesquisa/Autenticacao/Services/UserService.cs
@@ -17,7 +17,7 @@ namespace API.Pesquisa.Autenticacao.Services
         // users hardcoded for simplicity, store in a db with hashed passwords in production applications
         private List<User> _users = new List<User>
         {
-            new User { Id = "apiPesquisa_sulcapixaba",  Password = "api*uni176sul" }
+            new User { Id = "",  Password = "" }
         };
 
         public async Task<User> Authenticate(string username, string password)
